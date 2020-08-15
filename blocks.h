eebcb8f257151600d13c90ea68c878ad0b5b5874
Stewie410 <stewie410@gmail.com>
2020-08-15 16:46:49 -0400

    pretty and functional

--- blocks.h
+++ blocks.h
@@ -1,15 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-    // Icon     Command                             Update Interval     Update Signal
-    { "",      "sh -c ~/scripts/dwm/dwm-spotify.sh",     0,                  0 },
-    { "",       "sh -c ~/scripts/dwm/dwm-weather.sh",     1800,               1 },
-    { "📦",     "sh -c ~/scripts/dwm/dwm-updates.sh",     0,                  2 },
-    { "🌡",      "sh -c ~/scripts/dwm/dwm-cputemp.sh",     1,                  3 },
-    { "",      "sh -c ~/scripts/dwm/dwm-memory.sh",      1,                  4 },
-    { "",       "sh -c ~/scripts/dwm/dwm-network.sh",     0,                  5 },
-    { "",      "sh -c ~/scripts/dwm/dwm-pavolume.sh",    0,                  6 },
-    { "",       "sh -c ~/scripts/dwm/dwm-battery.sh",     30,                 7 },
-    { "",       "sh -c ~/scripts/dwm/dwm-datetime.sh",    30,                 8 },
+    // Icon     Command                                             Interval     Signal
+    { "",   "sh -c ~/scripts/dwm/dwm-spotify.sh | sed 's/^/  /;s/$/ /'",   0,           0 },
+    { "",   "sh -c ~/scripts/dwm/dwm-weather.sh | sed 's/^/  /;s/$/ /'",   1800,        1 },
+    { "",   "sh -c ~/scripts/dwm/dwm-updates.sh | sed 's/^/  /;s/$/ /'",   0,           2 },
+    { "",   "sh -c ~/scripts/dwm/dwm-cputemp.sh | sed 's/^/  /;s/$/ /'",   1,           3 },
+    { "",   "sh -c ~/scripts/dwm/dwm-memory.sh | sed 's/^/  /;s/$/ /'",    1,           4 },
+    { "",   "sh -c ~/scripts/dwm/dwm-network.sh | sed 's/^/  /;s/$/ /'",   0,           5 },
+    { "",   "sh -c ~/scripts/dwm/dwm-pavolume.sh | sed 's/^/  /;s/$/ /'",  0,           6 },
+    { "",   "sh -c ~/scripts/dwm/dwm-battery.sh | sed 's/^/  /;s/$/ /'",   30,          7 },
+    { "",   "sh -c ~/scripts/dwm/dwm-datetime.sh | sed 's/^/  /;s/$/ /'",  30,          8 },
 	//{"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
 	//{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 	//{"", "~/bin/statusbar/volume",						0,		10},
